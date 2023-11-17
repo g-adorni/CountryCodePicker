7dba4e802377cf4db9ac895cf75dab0a67c5920c
Gabor Adorni <gabor.adorni@gmail.com>
2023-11-17 12:29:17 +0100

    utils

---
+++ lib/src/utils.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../country_code_picker.dart';
+
+class Utils {
+  static void showStaticDialog(
+      BuildContext context, Function(CountryCode?) onChanged) async {
+    List<Map<String, String>> jsonList = codes;
+
+    List<CountryCode> elements =
+        jsonList.map((json) => CountryCode.fromJson(json)).toList();
+
+    final item = await showDialog(
+      context: context,
+      builder: (context) => Dialog(
+        child: SelectionDialog(
+          elements,
+          [],
+          showCountryOnly: false,
+          showFlag: true,
+          closeIcon: const Icon(Icons.close),
+        ),
+      ),
+    );
+    onChanged.call(item);
+  }
+}
